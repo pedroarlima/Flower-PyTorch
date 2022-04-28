717aa1de364509f0338552f81b30f8a3be5a45aa
Pedro Lima <pedroarlima@gmail.com>
2022-04-28 18:08:04 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,3 @@
 import flwr as fl
 
-fl.server.start_server(server_address="localhost::8080", config={"num_rounds": 3})
+fl.server.start_server(server_address="localhost:8080", config={"num_rounds": 3})
